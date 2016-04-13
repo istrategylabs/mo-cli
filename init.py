72fbe1040fcafb2be3c884f88eb1bdfa3b6d62d1
SarahJaine <sarahjaine.sz@gmail.com>
2016-04-13 10:02:54 -0400

    readability cleanup

--- init.py
+++ init.py
@@ -4,11 +4,19 @@ import subprocess
 @click.command()
 @click.argument('framework', type=str)
 def cli(framework):
+    """Starts cookiecuttter for new 'mo' project.
+    Type init 'some-framework' to get started."""
+    # format framework name into url string
     framework = framework.lower().replace(' ', '-')
     git_url = "https://github.com/istrategylabs/mo-"+framework
-    response = subprocess.check_output('curl -Is {0} | head -n 1'.format(git_url), shell=True)
+    response = subprocess.check_output(
+        'curl -Is {0} | head -n 1'.format(git_url),
+        shell=True)
     # convert response to string, native format is bytes
     if "HTTP/1.1 200 OK" in str(response):
         click.echo(response)
     else:
-        click.echo("No 'mo' project found for '{0}'.\nCheck https://github.com/istrategylabs for available 'mo' projects.".format(framework))
+        click.echo("No 'mo' project found for '{0}'.\n"
+                   "Check https://github.com/istrategylabs "
+                   "for available 'mo' projects."
+                   .format(framework))
