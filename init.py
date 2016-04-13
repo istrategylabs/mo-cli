74db8259a98e2cbaf92a98d97ed132cd1f82af00
SarahJaine <sarahjaine.sz@gmail.com>
2016-04-13 14:05:25 -0400

    added check/download for homebrew and cookiecutter; added cookiecutter run

--- init.py
+++ init.py
@@ -1,20 +1,31 @@
 import click
 import subprocess
+import requests
+
 
 @click.command()
 @click.argument('framework', type=str)
 def cli(framework):
-    """Starts cookiecuttter for new 'mo' project.
+    """Starts new 'mo' project.
     Type init 'some-framework' to get started."""
-    # format framework name into url string
+    # Format framework name into url string
     framework = framework.lower().replace(' ', '-')
     git_url = "https://github.com/istrategylabs/mo-"+framework
-    response = subprocess.check_output(
-        'curl -Is {0} | head -n 1'.format(git_url),
-        shell=True)
-    # convert response to string, native format is bytes
-    if "HTTP/1.1 200 OK" in str(response):
-        click.echo(response)
+    response = requests.get(git_url)
+    status = response.status_code
+    if status == 200:
+        # Check for homebrew, install if missing
+        homebrew_check = subprocess.run("which -s brew", shell=True)
+        if homebrew_check.returncode == 1:
+            subprocess.run('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"',
+                           shell=True)
+        # Check for cookiecutter, install if missing
+        brew_list = subprocess.check_output('brew list', shell=True)
+        if "cookiecutter" not in str(brew_list):
+            subprocess.run('brew install cookiecutter', shell=True)
+        # Start cookiecuter process for framework
+        subprocess.run('cookiecutter gh:istrategylabs/mo-{0}'
+                       .format(framework), shell=True)
     else:
         click.echo("No 'mo' project found for '{0}'.\n"
                    "Check https://github.com/istrategylabs "
