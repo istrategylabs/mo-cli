331ec5fa357d693a764388648594b5e7768c1a94
SarahJaine <sarahjaine.sz@gmail.com>
2016-04-14 17:40:09 -0400

    change subprocess method to work with python 2.7; remove install of homebrew; improved help message

--- init.py
+++ init.py
@@ -1,40 +1,46 @@
 import click
 import requests
 import subprocess
+import sys
 
 
+# Note: add verbose mode to see more error code
 @click.command()
 @click.argument("framework", type=str)
 def cli(framework):
-    """Starts new 'mo' cookiecutter template for ISL project.
-    Check https://github.com/istrategylabs for available 'mo' projects.\n
+    """Command-line utility to start projects from ISL cookiecutters.
+    Search https://github.com/istrategylabs repositories for 'mo' projects
+    to see available ISL cookiecutters.\n
     Type init "some-framework" to get started."""
-    # Format framework name, check for cookiecutter
+    # Check that ISL cookiecutter exists
+    isl_url = "https://github.com/istrategylabs"
     framework = framework.lower().replace(" ", "-")
-    git_url = "https://github.com/istrategylabs/mo-"+framework
-    response = requests.get(git_url)
+    mo_url = isl_url+"/mo-"+framework
+    response = requests.head(mo_url)
     status = response.status_code
-    if status == 200:
-        # Check for homebrew, install if missing
-        homebrew_check = subprocess.run("which -s brew", shell=True)
-        if homebrew_check.returncode != 0:
-            subprocess.run("/usr/bin/ruby -e '$(curl -fsSL "
-                           "https://raw.githubusercontent.com/"
-                           "Homebrew/install/master/install)'",
-                           shell=True)
-        # Check for cookiecutter, install if missing
-        brew_list = subprocess.check_output("brew list", shell=True)
-        if "cookiecutter" not in str(brew_list):
-            subprocess.run("brew install cookiecutter", shell=True)
-        # Check for user confirmation
-        if click.confirm("Ready to start cookiecutter gh:istrategylabs/mo-{0} "
-                         "in current directory.\n"
-                         "Do you want to continue?".format(framework)):
-            # Run cookiecutter
-            subprocess.run("cookiecutter gh:istrategylabs/mo-{0}"
-                           .format(framework), shell=True)
-    else:
+    # Note: add permisiion error, and some other commmon errors
+    if status != 200:
         click.echo("No 'mo' project found for '{0}'.\n"
-                   "Check https://github.com/istrategylabs "
-                   "for available 'mo' projects."
-                   .format(framework))
+                   "Check {1} for 'mo' projects.".format(isl_url, framework))
+    else:
+        # Check for cookiecutter, ask sto resolve if missing
+        cookie_check = subprocess.call("which -s cookiecutter", shell=True)
+        if cookie_check != 0:
+            brew_check = subprocess.call("which -s brew", shell=True)
+            if brew_check != 0:
+                click.echo("Mo-cli needs cookiecutter utility to continue. "
+                           "Check out https://github.com/audreyr/cookiecutter "
+                           "for installation instructions.")
+                sys.exit()
+            else:
+                if click.confirm("Mo-cli needs cookiecutter utility to "
+                                 "continue. "
+                                 "Allow brew install of cookiecutter:",
+                                 abort=True):
+                    subprocess.call("brew install cookiecutter", shell=True)
+        # Check for user confirmation before cookiecutter
+        if click.confirm("Ready to start cookiecutter gh:istrategylabs/mo-{0} "
+                         "in the current directory.\n"
+                         "Do you want to continue?".format(framework)):
+            subprocess.call("cookiecutter gh:istrategylabs/mo-{0}"
+                            .format(framework), shell=True)
