3c30c7f674be50a4a7fe5f31a3ff3e08a447e7f3
SarahJaine <sarahjaine.sz@gmail.com>
2016-04-14 14:52:09 -0400

    added setup args

--- setup.py
+++ setup.py
@@ -3,9 +3,20 @@ from setuptools import setup
 setup(
         name='mo-cli',
         version='1.0',
+        description='starts new "mo" cookiecutter template',
+        long_description='',
+        url='https://github.com/SarahJaine/mo-cli',
+        author='Sarah Jaine Szekeresh',
+        author_email='sarahjaine.sz@gmail.com',
+        liscense='',
+        classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.5',
+        ],
         py_modules=['init'],
         install_requires=[
             'Click',
+            'requests',
          ],
         entry_points='''
             [console_scripts]
