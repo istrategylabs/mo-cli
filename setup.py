667f11d1e158930381f936a3023fa09eb8ff7717
SarahJaine <sarahjaine.sz@gmail.com>
2016-04-18 15:45:49 -0400

    small updates

--- setup.py
+++ setup.py
@@ -1,23 +1,28 @@
 from setuptools import setup
 
+long_description = open('README.md').read()
+
 setup(
         name='mo-cli',
-        version='0.1.0',
+        version='0.1',
         description='starts new "mo" cookiecutter template',
-        long_description='',
+        long_description=long_description,
         url='https://github.com/SarahJaine/mo-cli',
         author='Sarah Jaine Szekeresh',
         author_email='sarahjaine@isl.co',
         liscense='MIT',
         classifiers=[
             'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Natural Language :: English',
             'Programming Language :: Python :: 3.5',
+            'Topic :: Software Development :: Libraries :: Python Modules',
         ],
         py_modules=['init'],
         install_requires=[
-            'click',
-            'requests',
-            'cookiecutter,'
+            'click==6.6',
+            'requests==2.9.1',
+            'cookiecutter==1.4.0',
          ],
         entry_points='''
             [console_scripts]
