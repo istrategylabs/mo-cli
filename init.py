5c24f0b2ce4e5ae95399aa4cc6e71505c7ee56ad
SarahJaine <sarahjaine.sz@gmail.com>
2016-04-18 13:53:16 -0400

    added error handling and more info to setup.py

--- init.py
+++ init.py
@@ -4,43 +4,53 @@ import subprocess
 import sys
 
 
-# Note: add verbose mode to see more error code
 @click.command()
-@click.argument("framework", type=str)
-def cli(framework):
-    """Command-line utility to start projects from ISL cookiecutters.
-    Search https://github.com/istrategylabs repositories for 'mo' projects
+@click.argument('framework', type=click.STRING)
+@click.option('--user', '-u',
+              help='Specify git account to use for cookiecutters',
+              default='istrategylabs')
+def cli(framework, user):
+    '''Command-line utility to start cookiecutter templates from GitHub repos.
+    Search https://github.com/istrategylabs 'mo' projects
     to see available ISL cookiecutters.\n
-    Type init "some-framework" to get started."""
-    # Check that ISL cookiecutter exists
-    isl_url = "https://github.com/istrategylabs"
-    framework = framework.lower().replace(" ", "-")
-    mo_url = isl_url+"/mo-"+framework
-    response = requests.head(mo_url)
-    status = response.status_code
-    # Note: add permisiion error, and some other commmon errors
+    Type init 'some-framework' to get started.'''
+
+    # Check that cookiecutter template exists for requested framework
+    git_url = 'https://github.com/'
+    framework = framework.lower().replace(' ', '-')
+    mo_url = git_url+user+'/mo-'+framework
+    try:
+        response = requests.head(mo_url)
+        status = response.status_code
+    except requests.exceptions.ConnectionError as e:
+        sys.exit("Encountered an error with the connection.")
+    except Exception as e:
+        sys.exit(e)
     if status != 200:
-        click.echo("No 'mo' project found for '{0}'.\n"
-                   "Check {1} for 'mo' projects.".format(isl_url, framework))
-    else:
-        # Check for cookiecutter, ask sto resolve if missing
-        cookie_check = subprocess.call("which -s cookiecutter", shell=True)
-        if cookie_check != 0:
-            brew_check = subprocess.call("which -s brew", shell=True)
-            if brew_check != 0:
-                click.echo("Mo-cli needs cookiecutter utility to continue. "
-                           "Check out https://github.com/audreyr/cookiecutter "
-                           "for installation instructions.")
-                sys.exit()
-            else:
-                if click.confirm("Mo-cli needs cookiecutter utility to "
-                                 "continue. "
-                                 "Allow brew install of cookiecutter:",
-                                 abort=True):
-                    subprocess.call("brew install cookiecutter", shell=True)
-        # Check for user confirmation before cookiecutter
-        if click.confirm("Ready to start cookiecutter gh:istrategylabs/mo-{0} "
-                         "in the current directory.\n"
-                         "Do you want to continue?".format(framework)):
-            subprocess.call("cookiecutter gh:istrategylabs/mo-{0}"
-                            .format(framework), shell=True)
+        sys.exit("No '{0}' cookiecutter found at {1}."
+                 .format(framework, git_url+user))
+
+    # Check for cookiecutter tool, install if missing
+    cookie_check = subprocess.call('which -s cookiecutter', shell=True)
+    if cookie_check != 0:
+        brew_check = subprocess.call('which -s brew', shell=True)
+        # Exit users if they don't have cookiecutter or homebrew
+        if brew_check != 0:
+            sys.exit('Mo-cli needs cookiecutter utility to continue. '
+                     'Check out https://github.com/audreyr/cookiecutter '
+                     'for installation instructions.')
+        if click.confirm('Mo-cli needs cookiecutter utility to continue. '
+                         'Allow brew install of cookiecutter?',
+                         abort=True):
+            subprocess.call('brew install cookiecutter', shell=True)
+
+    # Run cookiecutter template
+    if click.confirm('Ready to start cookiecutter gh:{0}/mo-{1} '
+                     'in the current directory.\n'
+                     'Do you want to continue?'.format(user, framework)):
+        try:
+            subprocess.check_call('cookiecutter gh:{0}/mo-{1}'
+                                  .format(user, framework), shell=True,
+                                  stderr=subprocess.DEVNULL)
+        except subprocess.CalledProcessError:
+            sys.exit("Problem encounted while cloning '{0}'.".format(mo_url))
--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@ from setuptools import setup
 
 setup(
         name='mo-cli',
-        version='1.0',
+        version='0.1.0',
         description='starts new "mo" cookiecutter template',
         long_description='',
         url='https://github.com/SarahJaine/mo-cli',
         author='Sarah Jaine Szekeresh',
-        author_email='sarahjaine.sz@gmail.com',
-        liscense='',
+        author_email='sarahjaine@isl.co',
+        liscense='MIT',
         classifiers=[
             'Intended Audience :: Developers',
             'Programming Language :: Python :: 3.5',
         ],
         py_modules=['init'],
         install_requires=[
-            'Click',
+            'click',
             'requests',
          ],
         entry_points='''
